26878bb8088d0d8b1b8550e7fa2c35af810910c6
Sophia Coelho <sophia.coelho@etec.sp.gov.br>
2021-06-22 16:42:39 +0000

    config framework/pag home

---
+++ public/application/views/home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta charset="viewport" content="width=device=width, initial-scale=1.0">
+	<title>XUXA</title>
+</head>
+<body>
+
+<p>Directory access is forbidden.</p>
+
+</body>
+</html>
